76f339ed16bcdf85509dfd820fee37c2872bdbf6
Ville Heikkilä <tehomail@gmail.com>
2019-12-25 02:06:28 +0200

    Add useFinnkino hook that gets given dates and places movies with title and url

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {useFinnkino, Event }from './services/finnkino'
 
 const App = () => {
+  const [data] = useFinnkino(1033, "27.12.2019")
+  console.log(data)
+
   return (
     <div>
-        Moi
+        {data.map((e: Event) =>  (<div> <a href={e.url} key={e.title}>{e.title}</a></div>))}
     </div>
   );
 }
---
+++ src/services/finnkino.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+const xml2js = require('xml2js')
+
+export interface Event {
+    title: string,
+    url: string,
+}
+export const useFinnkino = (id: Number, date: string) => {
+    const [data, setData] = useState<Event[]>([]);
+
+    useEffect(() => {
+        (async function() {
+            const response = await axios(`https://www.finnkino.fi/xml/Schedule/?area=${id}&dt=${date}`);
+            const { Schedule } = await xml2js.parseStringPromise(response.data)
+            const movies = Schedule.Shows[0].Show.flatMap((movie: any) => {return {title: movie.Title[0], url: movie.EventURL[0]}}).reduce((result: Event[], element: Event) => {
+                const duplicate = result.find( m => m.title === element.title )
+                return duplicate ? result : [...result, element];
+              }, []);
+            setData(movies)
+        })();
+    }, [id, date])
+      
+    return [data]
+}
